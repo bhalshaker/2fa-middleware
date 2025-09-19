693ede5a03fa01c633378ec2a94829c974c5fbdd
Ebrahim Alshaker <alshaker@proton.me>
2025-09-19 12:03:49 +0300

    Improve Settings class

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,18 +1,31 @@
 from dataclasses import dataclass
 import os
 
-@dataclass
+@dataclass(frozen=True)
 class Settings:
     issuer_name:str
     otp_size:int
-    pg_db_url:str
-    pg_db_pool_size:int
-    pg_db_max_overflow:int
+    pg_db_user:str
+    pg_db_password:str
+    pg_db_database:str
+    pg_db_host:str
+    pg_db_port:int
+    pg_db_pool_min_size:int
+    pg_db_pool_max_size:int
 
 
     @staticmethod
-    def from_env(cls):
-        return cls(
+    def from_env():
+        return Settings(
             issuer_name=os.getenv("ISSUER_NAME","2FA-MIDDLEWARE"),
-            otp_size=int(os.getenv("OTP_SIZE","6"))
+            otp_size=int(os.getenv("OTP_SIZE","6")),
+            pg_db_user=os.getenv("OTP_SIZE","6"),
+            pg_db_password=os.getenv("OTP_SIZE","6"),
+            pg_db_database=os.getenv("OTP_SIZE","6"),
+            pg_db_host=os.getenv("OTP_SIZE","6"),
+            pg_db_port=int(os.getenv("OTP_SIZE","6")),
+            pg_db_pool_min_size=int(os.getenv("OTP_SIZE","6")),
+            pg_db_pool_max_size=int(os.getenv("OTP_SIZE","6")),
         )
+
+settings=Settings.from_env()
