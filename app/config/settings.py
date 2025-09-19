3db6a2bf5c6fdcd91ea4cd7c4c060d0c5ea33c68
Ebrahim Alshaker <alshaker@proton.me>
2025-09-19 08:41:00 +0300

    Define Settings Class

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,2 +1,18 @@
+from dataclasses import dataclass
+import os
+
+@dataclass
 class Settings:
-    pass
+    issuer_name:str
+    otp_size:int
+    pg_db_url:str
+    pg_db_pool_size:int
+    pg_db_max_overflow:int
+
+
+    @staticmethod
+    def from_env(cls):
+        return cls(
+            issuer_name=os.getenv("ISSUER_NAME","2FA-MIDDLEWARE"),
+            otp_size=int(os.getenv("OTP_SIZE","6"))
+        )
