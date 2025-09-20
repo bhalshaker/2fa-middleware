1b3edfd973d61c040a5851d4d3936ff360ab472a
Ebrahim Alshaker <alshaker@proton.me>
2025-09-20 21:01:04 +0300

    Make DBs Pool connections related to FastAPI Lifecycle

--- app/main.py
+++ app/main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
-from app.dependacy.pg_database import create_db_pool,close_db_pool
+from app.dependacy.pg_database import create_pg_db_pool,close_pg_db_pool
+from app.dependacy.redis_database import close_redis_db_pool,create_redis_db_pool
+import logging
+
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    """Manages the database connection pool's lifecycle."""
-    await create_db_pool()
-    yield
-    await close_db_pool()
+    """Manages the databasees connection pool's lifecycle."""
+    # Connect to PostgreSQL and Redis
+    try:
+        await create_pg_db_pool()
+        await create_redis_db_pool()
+        logger.info("All database connections successfully created.")
+        yield  # The application serves requests here
+    except Exception as e:
+        print(f"Failed to connect to one or more databases: {e}")
+        # Optionally, you can handle shutdown here if startup fails
+        await close_pg_db_pool()
+        await close_redis_db_pool()
+        raise  # Re-raise the exception to stop the app from starting
+    finally:
+        # Disconnect from PostgreSQL and Redis on shutdown
+        logger.info("Application shutdown initiated...")
+        await close_pg_db_pool()
+        await close_redis_db_pool()
+        logger.info("All database connections successfully closed.")
