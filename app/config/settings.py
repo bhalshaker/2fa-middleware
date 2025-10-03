a54a8ca483dffa5651d441e1c59f564cbeebe90c
Ebrahim Alshaker <alshaker@proton.me>
2025-10-03 22:50:42 +0300

    Update Settings Class

--- app/config/settings.py
+++ app/config/settings.py
@@ -6,6 +6,7 @@ class Settings:
     # OTP Settings
     issuer_name:str
     otp_size:int
+    otp_life:int
     # Postgres Settings
     pg_db_user:str
     pg_db_password:str
@@ -24,12 +25,21 @@ class Settings:
     keycloack_client_secret:str
     keycloack_admin_username:str
     keycloack_admin_password:str
+    # Send Email Service API
+    resend_from_email:str
+    resend_api_url:str
+    resend_api_key:str
+    # Send SMS Service API
+    twilio_account:str
+    twilio_from_number:str
+    twilio_auth_token:str
 
     @staticmethod
     def from_env():
         return Settings(
             issuer_name=os.getenv("ISSUER_NAME","2FA-MIDDLEWARE"),
             otp_size=int(os.getenv("OTP_SIZE","6")),
+            otp_life=int(os.getenv("OTP_LIFE",10)),
             pg_db_user=os.getenv("PG_DB_USER","user"),
             pg_db_password=os.getenv("PG_DB_PASSWORD","password"),
             pg_db_database=os.getenv("PG_DB_DATABASE","6"),
@@ -40,11 +50,17 @@ class Settings:
             redis_url=os.getenv("REDIS_URL","redis://localhost:6379/0"),
             redis_max_connections=int(os.getenv("REDIS_MAX_CONNECTIONS","20")),
             keycloack_url=os.getenv("KEYCLOAK_URL","http://localhost:8080"),
-            keycloack_realm=os.getenv("KEYCLOAK_REALM","master"),
-            keycloack_client_id=os.getenv("KEYCLOAK_CLIENT_ID","admin-client"),
+            keycloack_realm=os.getenv("KEYCLOAK_REALM","2faproject"),
+            keycloack_client_id=os.getenv("KEYCLOAK_CLIENT_ID","fastapi-client"),
             keycloack_client_secret=os.getenv("KEYCLOAK_CLIENT_SECRET","secret"),
             keycloack_admin_username=os.getenv("KEYCLOAK_ADMIN_USERNAME","admin"),
             keycloack_admin_password=os.getenv("KEYCLOAK_ADMIN_PASSWORD","admin-password"),
+            resend_from_email=os.getenv("RESEND_FROM_EMAIL","2FA-Middleware <onboarding@resend.dev>"),
+            resend_api_url=os.getenv("RESEND_API_URL","https://api.resend.com/emails"),
+            resend_api_key=os.getenv("RESEND_API_KEY",""),
+            twilio_account=os.getenv("TWILIO_ACCOUNT",""),
+            twilio_from_number=os.getenv("TWILIO_FROM_NUMBER",""),
+            twilio_auth_token=os.getenv("TWILIO_AUTH_TOKEN","")
         )
 
 settings=Settings.from_env()
