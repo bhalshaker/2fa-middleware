901a43a7a15220d12ad90d701cc6f7d9c147c967
Ebrahim Alshaker <alshaker@proton.me>
2025-10-04 06:26:37 +0300

    Define User Profile Repository Main operations

--- app/repository/user_repo.py
+++ app/repository/user_repo.py
@@ -1,10 +1,76 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.model.user_profile import UserProfile
+from sqlalchemy.future import select
 
 class UserRepository():
-    @staticmethod
-    async def get_user_by_username(username:str,db: AsyncSession):
-        pass
+    """Repository for user profile related database operations."""
 
     @staticmethod
-    async def update_user_totp(username:str,seed:str,db: AsyncSession):
-        pass
+    async def get_user_by_username(username:str,db: AsyncSession)->UserProfile | None:
+        """
+         Retrieve a user by username.
+         Args:
+            username (str): The username of the user to retrieve.
+            db (AsyncSession): The database session. 
+        Returns:
+            UserProfile | None: The user profile if found, otherwise None.
+        """
+        result = await db.execute(select(UserProfile).where(UserProfile.username == username))
+        return result.scalars().first()
+
+    @staticmethod
+    async def update_user_totp(username:str,seed:str,db: AsyncSession)->UserProfile|None:
+        """
+        Update user totp.   
+        Args:
+            username (str): The username of the user to retrieve.
+            seed (str): TOTP seed secret key
+            db (AsyncSession): The database session.
+        Returns:
+            UserProfile | None: The user profile if found, otherwise None.
+        """
+        user = await UserRepository.get_user_by_username(username, db)
+        if user:
+            user.totp_secret_encrypted = seed
+            user.is_totp_verified = True
+            await db.commit()
+            await db.refresh(user)
+        return user
+    
+    @staticmethod
+    async def update_user_email(username:str,email:str,db: AsyncSession)->UserProfile|None:
+        """
+        Update user totp.   
+        Args:
+            username (str): The username of the user to retrieve.
+            email (str): New email address of the user
+            db (AsyncSession): The database session.
+        Returns:
+            UserProfile | None: The user profile if found, otherwise None.
+        """
+        user = await UserRepository.get_user_by_username(username, db)
+        if user:
+            user.email_address=email
+            user.is_email_address_verified = True
+            await db.commit()
+            await db.refresh(user)
+        return user
+    
+    @staticmethod
+    async def update_user_mobile(username:str,mobile:str,db: AsyncSession)->UserProfile|None:
+        """
+        Update user totp.   
+        Args:
+            username (str): The username of the user to retrieve.
+            movile (str): New mobile number of the user
+            db (AsyncSession): The database session.
+        Returns:
+            UserProfile | None: The user profile if found, otherwise None.
+        """
+        user = await UserRepository.get_user_by_username(username, db)
+        if user:
+            user.mobile_number=mobile
+            user.is_mobile_number_verified = True
+            await db.commit()
+            await db.refresh(user)
+        return user
