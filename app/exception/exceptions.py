bcf53a41c98bd3062078dfa46b63c2be48cdeead
Ebrahim Alshaker <alshaker@proton.me>
2025-10-04 17:25:06 +0300

    Finish UserProfileController and update all dependancies related to it

--- app/exception/exceptions.py
+++ app/exception/exceptions.py
@@ -36,3 +36,56 @@ class NoMatchingUserError(Exception):
         if self.code:
             return f"Error Code {self.code}: {self.message}"
         return self.message
+
+class NoMatchingOTPError(Exception):
+    """No Matching OTP Error class."""
+
+    def __init__(self, message="OTP could not be matched.", code=None):
+        self.message = message
+        self.code = code
+        super().__init__(self.message) # Call the base Exception constructor
+
+    def __str__(self):
+        if self.code:
+            return f"Error Code {self.code}: {self.message}"
+        return self.message
+    
+class NoMatchingSeedError(Exception):
+    """No Matching Seed Error class."""
+
+    def __init__(self, message="Seed could not be matched.", code=None):
+        self.message = message
+        self.code = code
+        super().__init__(self.message) # Call the base Exception constructor
+
+    def __str__(self):
+        if self.code:
+            return f"Error Code {self.code}: {self.message}"
+        return self.message
+
+class SeedWaitingForConfirmationError(Exception):
+    """Seed Waiting for Confirmation Error class."""
+
+    def __init__(self, message="There is already and existing seed witing for confirmation.", code=None):
+        self.message = message
+        self.code = code
+        super().__init__(self.message) # Call the base Exception constructor
+
+    def __str__(self):
+        if self.code:
+            return f"Error Code {self.code}: {self.message}"
+        return self.message
+
+class TechnicalError(Exception):
+    """Technical Error class."""
+
+    def __init__(self, message="Operation Failed Due to a Technical Error, Please contact administrator.", code=None):
+        self.message = message
+        self.code = code
+        super().__init__(self.message) # Call the base Exception constructor
+
+    def __str__(self):
+        if self.code:
+            return f"Error Code {self.code}: {self.message}"
+        return self.message
+ 
--- app/schema/user.py
+++ app/schema/user.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing_extensions import Self
 from typing import Optional
 from pydantic import BaseModel,EmailStr,model_validator,constr
+from pydantic import Field
 from uuid import uuid4
 
 @dataclass
@@ -18,21 +19,26 @@ class KeycloakUserInfo():
     mobile_verified:Optional[bool]=None
 
 class UserProfileInfo(BaseModel):
-    username:str
-    first_name:str
-    last_name:str
-    email:str
+    """Class to hold user profile information."""
+    username:Optional[str]=None
+    first_name:Optional[str]=None
+    last_name:Optional[str]=None
+    email:Optional[str]=None
     email_verified:Optional[bool]=None
     mobile:Optional[str]=None
     mobile_verified:Optional[bool]=None
+
+class UserProfileInfoResponse(BaseModel):
+    """Response model for user profile information."""
+    successful:bool
+    message:Optional[str]=None
+    user_profile_info=Optional[UserProfileInfo]=None
 
 
 class UpdateUserInfo(BaseModel):
     """Model to update user information"""
 
-    username:str
     email:Optional[EmailStr]=None
-    from pydantic import Field
     mobile: Optional[constr] = Field(default=None, regex=r'^\+973\d{8}$')
 
     @model_validator(mode='after')
@@ -42,6 +48,11 @@ class UpdateUserInfo(BaseModel):
         if not self.email and not self.mobile:
             raise ValueError("At least one of 'email' or 'mobile' must be provided.")
         return self
+class UpdateUserInfoResponse(BaseModel):
+    successful:bool
+    message:Optional[str]=""
+    email_update_requested:Optional[bool]=False
+    mobile_update_requested:Optional[bool]=False
 
 @dataclass
 class UpdateUserResults():
